1f5182bbe84b636414b10e3f53733af7d8087d6a
Alex Flores Z <lexzum10@gmail.com>
2024-02-20 12:41:35 -0500

    Add first verions

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,50 @@
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Autoplay,
+  A11y
+} from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/swiper-bundle.css'
+
+const images = [
+  'https://a.storyblok.com/f/95455/1350x1080/3ef7748922/p5240368.jpg',
+  'https://a.storyblok.com/f/95455/1350x1080/231d2407ea/p4030184.jpg',
+  'https://a.storyblok.com/f/95455/1350x1080/4aee2a00f4/p5240221.jpg'
+]
+
+export default function Carousel() {
+  return (
+    <Swiper
+      className="h-60 w-full"
+      modules={[Navigation, Pagination, Scrollbar, Autoplay, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      autoplay={{ delay: 2000 }}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <img className="w-full h-full object-cover" src={image} alt="" />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
+
+/* 
+      <img
+        class="w-60 shadow-2xl rounded-xl relative z-10"
+        src="https://ritzudev.netlify.app/profile.jpg"
+        alt=""
+      />
+      <img
+        class="w-60 shadow-2xl rounded-xl blur-lg absolute z-0 inset-0"
+        src="https://ritzudev.netlify.app/profile.jpg"
+        alt=""
+      />
+*/
